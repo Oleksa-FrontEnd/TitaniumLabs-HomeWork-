027190702b9d4837a3e06e8e1ebc46756705371d
-=EL=- <IT0671581471@gmail.com>
2020-11-08 22:24:42 +0200

    max min sum by function

--- HW1/index.js
+++ HW1/index.js
@@ -1,30 +1,31 @@
-/*maximum number in the array*/
-var numbersArray = [1,NaN,8,5,-3];
-var maxNumber = numbersArray[0];
-
-for (i = 0; i < numbersArray.length; i++) {
-    while (maxNumber < numbersArray[i]) {
-        maxNumber = numbersArray[i]
+const someFunction = ( arr ) => {
+    let maxNumber = arr[0];
+    let minNumber = arr[0];
+    let sumArr = 0;
+    for (i = 0; i < arr.length; i++){
+        if (typeof(arr[i]) === "number"){
+            if (maxNumber < arr[i]) {
+                maxNumber = arr[i]
+            }  else if (minNumber > arr[i]) {
+                minNumber = arr[i]
+            }
+                sumArr +=arr[i]
+        }
     }
+    return { min:minNumber, max:maxNumber, sum:sumArr}
 }
-console.log(maxNumber)
 
-/*minimum number in the array*/
-let numbers = [5,7,2,13];
-let minNumber = numbers[0];
-
-for (i = 0; i < numbers.length; i++) {
-    while (minNumber > numbers[i]){
-        minNumber = numbers[i]
-    }
-}
-console.log(minNumber)
-
-/*sum of an array of numbers*/
-let arrayNumber = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-let sumArray = 0;
-
-for (i = 0; i < arrayNumber.length; i++) {
-    sumArray += arrayNumber[i]
-}
-console.log(sumArray);
+let a_v2 = [3,"1112",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"1112"],
+    b_v2 = [-1,-8,-2,"1112"],
+    c_v2 = [1,7,3],
+    d_v2 = [1,undefined,3,5,-3],
+    e_v2 = [1,"1112",NaN,"1112",3,5,-3],
+    f_v2 = ['1',1.23, 'undefined', 'qwe'],
+    j_v2 = [undefined, 'test'];
+console.log(someFunction(a_v2));
+console.log(someFunction(b_v2));
+console.log(someFunction(c_v2));
+console.log(someFunction(d_v2));
+console.log(someFunction(e_v2));
+console.log(someFunction(f_v2));
+console.log(someFunction(j_v2));

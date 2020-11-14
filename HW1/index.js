2496e8bd216130bf7528e8e8d8be13d28dcf9270
-=EL=- <IT0671581471@gmail.com>
2020-11-14 22:05:04 +0200

    if(isNaN(arr[i]) || typeof(arr[i]) == 'string')

--- HW1/index.js
+++ HW1/index.js
@@ -5,18 +5,18 @@ const someFunction = ( arr ) => {
 
 
     for (i = 0; i < arr.length; i++) {
-        if(typeof arr[i] === 'number'){
-            sumArr +=arr[i]
-        } else {return null}
+        if(isNaN(arr[i]) || typeof(arr[i]) == 'string'){
+            return null
+        } else {sumArr +=arr[i]}
     }
     return { min:minNumber, max:maxNumber, sum:sumArr}
 }
 
-let a_v2 = [3,"1112",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"1112"],
+let a_v2 = [3,"11132",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"1112"],
     b_v2 = [-1,-8,-2,"1112"],
     c_v2 = [1,7,3],
     d_v2 = [1,undefined,3,5,-3],
-    e_v2 = [1,"1112",NaN,"1112",3,5,-3],
+    e_v2 = [1,"11132",NaN,"1112",3,5,-3],
     f_v2 = ['1',1.23, 'undefined', 'qwe'],
     j_v2 = [undefined, 'test'];
 console.log(someFunction(a_v2));

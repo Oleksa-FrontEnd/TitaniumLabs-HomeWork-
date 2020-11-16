e559ed8b356a10bb328e957a8f502544a1c691c2
-=EL=- <IT0671581471@gmail.com>
2020-11-16 22:03:59 +0200

    with empty arrays problem

--- HW1/index.js
+++ HW1/index.js
@@ -1,20 +1,26 @@
 const someFunction = ( arr ) => {
-    let maxNumber = Math.max(...arr);
-    let minNumber = Math.min(...arr);
-    let sumArr = 0;  //= arr.reduce(function(a,b){return(a+b)});
-
-    for (i = 0; i < arr.length; i++) {
-        if(isNaN(arr[i]) || typeof(arr[i]) == 'string'){
-            return null
-        } else {sumArr +=arr[i]}
+    let newArr = arr.filter(Number);
+    //console.log (newArr);
+    function deleteString(value) {
+        return (typeof value === 'number');
     }
+
+    let newArrayNumber = newArr.filter(deleteString);
+    //console.log(newArrayNumber)
+
+        let minNumber = Math.min(...newArrayNumber);
+        let maxNumber = Math.max(...newArrayNumber);
+        let sumArr = null;
+            for (i = 0; i < newArrayNumber.length; i++){
+                sumArr += newArrayNumber[i]
+            }
     return { min:minNumber, max:maxNumber, sum:sumArr}
 }
 
-let a_v2 = [3,"11132",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"1112"],
+let a_v2 = ["555","112",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"112"],
     b_v2 = [-1,-8,-2,"1112"],
-    c_v2 = [1,7,3],
-    d_v2 = [1,undefined,3,5,-3],
+    c_v2 = [1,-7,3],
+    d_v2 = [1,3,5,-3],
     e_v2 = [1,"11132",NaN,"1112",3,5,-3],
     f_v2 = ['1',1.23, 'undefined', 'qwe'],
     j_v2 = [undefined, 'test'];

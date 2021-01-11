010ebb85a9a94ca222e939da9f4ef3d2aba24842
-=EL=- <IT0671581471@gmail.com>
2021-01-12 00:04:52 +0200

    developing progress-bar for tamagotchi

--- HW3 (Tamagotchi)/version 2/script_V2.js
+++ HW3 (Tamagotchi)/version 2/script_V2.js
@@ -1,8 +1,60 @@
+function userProgress(time){
+    let start = 0;
+    var time = Math.round(time * 1000/100);
+    let progressElement = document.getElementById('user-progress');
+    let intervalId = setInterval(function (){
+        if (start > 100){
+            clearInterval(intervalId);
+            userProgressCallBack();
+        } else {
+            progressElement.value = start;
+            start = start + 5;
+        }
+    }, time)
+}
+function userProgressCallBack(){
+    document.querySelector('.hidden-block').style.display = 'block'
+}
+userProgress(10);
+
+/*
 let health = 42;
-let satiety = 100;
+let satiety = 100;//сытость
 let joy = 100;  //радость
 let purity = 100; //чистота
+*/
+/*ProgressBar*/
+/*
+let time2 = setInterval(function life (){
+    if (health <= 0){
+        health = 'pet died';
+        clearInterval(time)
+        document.getElementById("health").innerHTML = health;
+    } else{
+        --health;
+        var elem = document.getElementById('health');
+        elem.style.healtth = health + '%';
+        document.getElementById("health").innerHTML = health + '%';
+    }
+}, 500)
 
+function move(){
+    var elem = document.getElementById('myBar');
+    var width = 42;
+    var id = setInterval(frame, 50);
+    function frame(){
+        if (width >= 100){
+            clearInterval(id)
+        } else{
+            width = width + 1;
+            elem.style.width = width + '%';
+            document.getElementById('label').innerHTML = width + '%';
+        }
+    }
+}
+*/
+/*Health*/
+/*
 function cure() {
     if (health < 100){
         health = health + 5;
@@ -18,27 +70,14 @@ let time = setInterval(function life (){
             document.getElementById("health").innerHTML = health;
         } else{
             --health;
-            document.getElementById("health").innerHTML = health;
-        }
-    }, 50)
+            var elem = document.getElementById('health');
+            elem.style.healtth = health + '%';
+            document.getElementById("health").innerHTML = health + '%';
 
-/*progressBar
-function move(){
-    var elem = document.getElementById('myBar');
-    var width = 60;
-    var id = setInterval(frame, 50);
-    function frame(){
-        if (width >= 100){
-            clearInterval(id)
-        } else{
-            width = width + 1;
-            elem.style.width = width + '%';
-            document.getElementById('label').innerHTML = width + '%';
+
         }
-    }
-}
-setInterval(function life (){
-    --width;
-    document.getElementById("width").innerHTML = width;
-}, 1000)
+    }, 100)
 */
+
+
+

37a968e57a8a16494b609a4f418049ff4da46a18
-=EL=- <IT0671581471@gmail.com>
2021-01-26 23:54:24 +0200

    creation of MyMethods

--- HW3 (Tamagotchi)/myMethods/methods.js
+++ HW3 (Tamagotchi)/myMethods/methods.js
@@ -1,4 +1,31 @@
 /* forEach */
 for (let i = 0; i < arr.length; i++){
-    console.log(i);
+    console.log(arr[i]);
+}
+for (let arr of array){
+    console.log(arr)
+}
+
+/* map */
+let arr = [3, 5, 42];
+
+let myMap = function (){
+    for (let i = 0; i < arr.length; i++){
+        console.log(arr[i]*2);
+    }
+}
+myMap(arr);
+
+/* sort */
+let mySort = function compare(a, b) {
+    //a меньше b по некоторому критерию сортировки
+    if (a < b) {
+        return -1;
+    }
+    //a больше b по некоторому критерию сортировки
+    if (a > b) {
+        return 1;
+    }
+    // a должно быть равным b
+    return 0;
 }

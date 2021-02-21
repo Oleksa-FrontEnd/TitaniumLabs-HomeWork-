6177924456a9fa1461e7843aee4a981a599bfcfd
-=EL=- <IT0671581471@gmail.com>
2021-02-21 22:45:11 +0200

    get, set, privat and protected

--- HW3 (Tamagotchi)/version 3/class/get_set/get_set.js
+++ HW3 (Tamagotchi)/version 3/class/get_set/get_set.js
@@ -2,11 +2,16 @@ class User {
     constructor() {
        // this._name = name;
     }
+    #test = 'Hohoho'
     set name(name){
+        console.log('Private = '+this.#test)
         this._name = name.trim().toLowerCase();
     }
     get name(){
         return this._name;
+    }
+    one(){
+        console.log(this.#test)
     }
 }
 const student = new User();
@@ -14,3 +19,9 @@ student.name = '     IvAn     ';
 //student.set('    Ivan');
 console.log(student);
 console.log(student.name);
+//console.log(student.#test);
+
+class User2 extends User{};
+const student2 = new User2();
+student2.one();
+student2.name = 'Oleg';

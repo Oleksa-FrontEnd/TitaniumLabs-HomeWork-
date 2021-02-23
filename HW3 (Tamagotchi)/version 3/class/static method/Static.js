6bf97a75daf299703d9d83c6bedfa0377fb6fa1f
-=EL=- <IT0671581471@gmail.com>
2021-02-23 22:49:04 +0200

    this

--- HW3 (Tamagotchi)/version 3/class/static method/Static.js
+++ HW3 (Tamagotchi)/version 3/class/static method/Static.js
@@ -1,5 +1,6 @@
 //static Method
 class User {
+    static test = true;
     constructor(name) {
         this.name = name;
     }
@@ -19,5 +20,13 @@ const person = new User('Ivan');
 console.log(person);
 
 console.log(User.getRole('alex@ex'));
+console.log(User.test);
 //console.log(person.getRole());
-
+class Student extends User {
+    static test = false;
+    constructor(name) {
+        super();
+    }
+}
+console.log(Student.getRole('test'));
+console.log(Student.test);
---
+++ HW3 (Tamagotchi)/version 3/class/this/this.js
@@ -0,0 +1,43 @@
+console.log(this);//window
+//arrow function => this => window
+
+//
+document.querySelector('.b-1').addEventListener('click', f1);
+
+function f1() {
+    console.log(this);
+    this.style.background = 'orange';
+}
+
+let p = document.querySelectorAll('p');
+for (let i = 0; i < p.length; i++) {
+    p[i].onclick = f1;
+}
+class U2 {
+    constructor(model) {
+        this.model = model;
+    }
+    showThis() {
+        console.log(this);
+    }
+}
+console.log(U2)
+
+const boat = new U2('42');
+const boat2 = new U2('4242');
+console.log(boat);
+console.log(boat2);
+boat.showThis();
+boat2.showThis();
+
+class YellowSubmarine extends  U2 {
+    constructor(model, color) {
+        super(model);
+        this.color = color;
+        console.log('__________________');
+        console.log(this.model);
+    }
+}
+let b = new YellowSubmarine(422, 'red');
+console.log(b);
+console.log(b.model);
